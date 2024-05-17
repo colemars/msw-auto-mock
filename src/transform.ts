78372a37726642775083278d1f78aefbef9dd8f3
Cole Marsteller <cole.marsteller@umbra.space>
2024-05-17 01:34:12 -0500

    WIP

--- src/transform.ts
+++ src/transform.ts
@@ -72,7 +72,7 @@ export function transformToHandlerCode(operationCollection: OperationCollection)
       return `http.${op.verb}(\`\${baseURL}${op.path}\`, () => {
         const localData = fetchLocalData(\`\${op.path}\`);
         if (localData) {
-          return HttpResponse.json(localData[op.verb]);
+          // return HttpResponse.json(localData[op.verb]);
         }
         
         const resultArray = [${op.response.map(response => {
